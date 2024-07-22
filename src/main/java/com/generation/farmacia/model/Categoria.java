da054215b7cb4d45783c824a324a36fb8bf8cf60
Weida Winy <winnyweida@hotmail.com>
2024-07-22 12:58:50 -0300

    Criacao do crud das categorias
    - Criei as classes model, repository e controller para a realização dos 6 métodos do recurso categoria.
    GetAll, GetById, GetByNome, Put, Post e Delete

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,41 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome da categoria é obrigatório!")
+	@Size(min = 3, max = 100, message = "O nome deve conter entre 3 a 100 caracteres")
+	private String nomeCategoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeCategoria() {
+		return nomeCategoria;
+	}
+
+	public void setNomeCategoria(String nomeCategoria) {
+		this.nomeCategoria = nomeCategoria;
+	}
+	
+	
+
+}
